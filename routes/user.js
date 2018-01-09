7bc2d08378af6fa142ec8b71f92e699f11501147
Dorothysun <dsun@wellesley.edu>
2018-01-09 13:21:12 -0500

    finish the user name query

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,12 @@ const express = require('express');
 var users = require('../userStorage.js');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-  res.send('these are the users');
+router.get('/:name', function (req, res, next) {
+  // console.log(req.query());
+  var name = req.params.name
+  console.log(name);
+  console.log(users.getUserByName(name));
+  res.send(users.getUserByName(name));
 });
 
 module.exports = router
